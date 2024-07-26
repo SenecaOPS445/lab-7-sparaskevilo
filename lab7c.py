c4eca48133553592871ab34a8d5dbcf6ff7098d0
Sandra Paraskevilo <sparaskevilo.myseneca.ca>
2024-07-26 02:08:00 -0400

    Typed out time_to_sec and sec_to_time functions

---
+++ lab7c.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# Student ID: [seneca_id]
+class Time:
+    """Simple object type for time of the day.
+    data attributes: hour, minute, second
+    """
+    def __init__(self,hour=12,minute=0,second=0):
+        """constructor for time object""" 
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+
+def format_time(t):
+    """Return time object (t) as a formatted string"""
+    return f'{t.hour:02d}:{t.minute:02d}:{t.second:02d}'
+
+def sum_times(t1, t2):
+    """Add two time objests and return the sum."""
+    sum = Time(0,0,0)
+    sum.hour = t1.hour + t2.hour
+    sum.minute = t1.minute + t2.minute
+    sum.second = t1.second + t2.second
+
+    #If seconds/minutes is 60 or more subtract 60 and add 1 to the minute/hour
+    if sum.second >= 60:
+        quotient = sum.second // 60 
+        while quotient > 0:
+            quotient = quotient - 1 
+            sum.second -= 60 
+            sum.minute += 1
+    if sum.minute >= 60:
+        quotient = sum.minute // 60  # Floor division to reutrn a rounded number less than or equal to the result.
+        while quotient > 0: #  Keep track how many times we need to subtract by 60 and add to minute/hour
+            quotient = quotient - 1 
+            sum.minute -= 60 # Stores the result of subtracting 60 in sum.minute
+            sum.hour += 1 # Add to sum.hour by one
+        
+    return sum
+
+def change_time(time, seconds):
+    '''Modify time attribute by adding seconds'''
+    time.second += seconds
+    if valid_time(time) != True:
+        while time.second >= 60:
+            time.second -= 60
+            time.minute += 1
+        while time.second < 0:
+            time.minute -= 1
+            time.second += 60
+        while time.minute >= 60:
+            time.minute -= 60
+            time.hour += 1
+        while time.minute < 0:
+            time.hour -= 1
+            time.minute += 60
+    return None
+
+def time_to_sec(time):
+    '''convert a time object to a single integer representing the number of seconds from mid-night'''
+    minutes = time.hour * 60 + time.minute 
+    seconds = minutes * 60 + time.second
+    return seconds
+
+def sec_to_time(seconds):
+    '''convert a given number of seconds to a time object in hour,minute,second format'''
+    time = Time()
+    minutes, time.second = divmod(seconds, 60) # Returns a tuple with quotient and remainder from seconds / 60
+    time.hour, time.minute = divmod(minutes, 60) # Returns a tuple with quotient and remainder from minutes / 60
+    return time
+
+def valid_time(t):
+    """check for the validity of the time object attributes:
+        24 > hour > 0, 60 > minute > 0, 60 > second > 0 """
+    if t.hour < 0 or t.minute < 0 or t.second < 0:
+        return False
+    if t.minute >= 60 or t.second >= 60 or t.hour >= 24:
+        return False
+    return True
